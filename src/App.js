a4d9adc20dd25dd788c734568e783d3b8d39c6cb
leesebas <leesebas@amazon.com>
2020-12-02 12:03:35 +0800

    update 3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now 2
+          Learn React Now 3
         </a>
       </header>
     </div>
