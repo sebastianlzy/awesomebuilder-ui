2c04d47e367f527e2ed7748f91d837db01e67cee
leesebas <leesebas@amazon.com>
2020-12-15 18:03:09 +0800

    retrieve instance hostnames

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const get = require('lodash/get')
+const AWS = require('aws-sdk');
+const got = require('got');
 
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const AWS = require('aws-sdk');
 AWS.config.update({region: 'ap-southeast-1'});
+const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
-const got = require('got');
+
 
 const getHostname = async () => {
     try {
@@ -24,11 +27,53 @@ const getHostname = async () => {
     }
 }
 
+const extractHostNames = (data) => {
+    const reservations = get(data, 'Reservations', [])
+    return reservations.reduce((hostnames, reservation) => {
+        if (get(reservation, 'Instances.0.State.Name') !== "running") {
+            return hostnames
+        }
+
+        if (get(reservation, 'Instances.0.ImageId') !== "ami-0ae5ba72ac157d468") {
+            return hostnames
+        }
+
+        hostnames.push(get(reservation, 'Instances.0.PrivateDnsName'))
+        return hostnames
+    }, [])
+
+}
+
+const getHostNames = async () => {
+    return new Promise((resolve, reject) => {
+        ec2.describeInstances({
+            DryRun: false
+        }, function(err, data) {
+            console.log("error", err)
+            if (err) {
+                reject(err.stack)
+            } else {
+                console.log("Success", JSON.stringify(data));
+                resolve(extractHostNames(data))
+            }
+        });
+    })
+}
+
 app.get('/api/get-instance-hostname', (req, res) => {
     return getHostname().then((hostname) => {
         res.send({hostname: `Hello From ${hostname}`});
     }).catch(() => {
         res.send({hostname: 'Did not send through'})
+    })
+});
+
+app.get('/api/get-all-instance-hostnames', (req, res) => {
+    return getHostNames().then((hostnames) => {
+        console.log("hostnames", hostnames)
+        res.send({hostnames: hostnames});
+    }).catch(() => {
+        res.send({hostnames: []})
     })
 });
 
--- src/App.js
+++ src/App.js
@@ -1,44 +1,82 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
-class App extends Component {
-  state = {
-    response: '',
-    post: '',
-  };
-  componentDidMount() {
-    this.callApi()
-        .then(res => this.setState({ response: res.hostname }))
-        .catch(err => this.setState({ response: JSON.stringify(err) }));
-  }
-  callApi = async () => {
-    const response = await fetch('/api/get-instance-hostname');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    console.log(body)
-    return body;
-  };
-  render() {
-    return (
-        <div className="App">
-          <p>{this.state.response}</p>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+import {Grid, Paper} from '@material-ui/core'
+import {withStyles} from '@material-ui/core/styles';
+import ec2Icon from './ec2-icon.png'
 
-        </div>
-    );
-  }
+const useStyles = (theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+});
+
+class App extends Component {
+    state = {
+      currHostName: '',
+      instanceHostnames: [],
+    };
+
+    componentDidMount() {
+        this.getCurrHostName()
+            .then(res => this.setState({currHostName: res.hostname}))
+            .catch(err => this.setState({currHostName: JSON.stringify(err)}));
+
+        this.getHostNames()
+            .then(res => this.setState({instanceHostnames: res.hostnames}))
+            .catch(err => this.setState({instanceHostnames: [JSON.stringify(err)]}));
+    }
+
+    getCurrHostName = async () => {
+        const currHostName = await fetch('/api/get-instance-hostname');
+        const body = await currHostName.json();
+        if (currHostName.status !== 200) throw Error(body.message);
+        console.log(body)
+        return body;
+    };
+
+    getHostNames = async () => {
+        const currHostName = await fetch('/api/get-all-instance-hostnames');
+        const body = await currHostName.json();
+        if (currHostName.status !== 200) throw Error(body.message);
+        console.log(body)
+        return body;
+    };
+
+    renderGrids = (gridValues) => {
+      const grids = []
+      for (let i = 0; i< gridValues.length; i++) {
+        grids.push(
+            <Grid item xs key={`homepage-grid-${i}`}>
+              <Paper className={this.props.classes.paper}>
+                <img src={ec2Icon} alt="ec2 logo" />
+                <p>{gridValues[i]}</p>
+              </Paper>
+            </Grid>
+        )
+      }
+      return grids
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <p>{this.state.currHostName}</p>
+                <Grid container spacing={3}>
+                  {this.renderGrids(this.state.instanceHostnames)}
+                </Grid>
+            </div>
+        );
+    }
 }
-export default App;
+
+App.propTypes = {
+    classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(useStyles)(App);
