8d3055a769c2781021eef48a75c2b5ce4c3e7435
leesebas <leesebas@amazon.com>
2020-12-15 17:04:52 +0800

    Retrieve hostname from node server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ express: 'Hello From Express Server 1' });
-});
-app.post('/api/world', (req, res) => {
-    console.log(req.body);
-    res.send(
-        `I received your POST request. This is what you sent me: ${req.body.post}`,
-    );
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'ap-southeast-1'});
+
+const got = require('got');
+
+const getHostname = async () => {
+    try {
+        const response = await got(
+            'http://169.254.169.254/latest/meta-data/hostname',
+            {timeout: 500}
+        );
+        return response.body
+    } catch (error) {
+        console.log(error)
+        return error.response.body;
+    }
+}
+
+app.get('/api/get-instance-hostname', (req, res) => {
+    return getHostname().then((hostname) => {
+        res.send({hostname: `Hello From ${hostname}`});
+    }).catch(() => {
+        res.send({hostname: 'Did not send through'})
+    })
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
