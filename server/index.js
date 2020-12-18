e39dd39c37fde6d15ada1a5632a65da7d1cc5c94
leesebas <leesebas@amazon.com>
2020-12-18 15:30:00 +0800

    log req

--- server/index.js
+++ server/index.js
@@ -87,6 +87,7 @@ const getHostNames = async () => {
 
 app.get('/api/get-instance-hostname', (req, res) => {
     const insertHostName = async (hostname) => {
+        console.log(req)
         console.log(`[${req.get('origin')}] get-instance-hostname: `, hostname)
         return new Promise((resolve, reject) => {
             pool.query(`INSERT into hostname(name) values("${hostname}") `, function (error, results, fields) {
